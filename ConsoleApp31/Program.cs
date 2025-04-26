c0749e84242769be0a4c2317f7979644b8b40540
user <user@XEDICEZEYNALOVA>
2025-04-26 19:12:14 +0400

    Add project files.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp31
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("n ve s deyerlerini daxil edin (arada bosluq qoy):");
+            string[] input = Console.ReadLine().Split();
+            int n = Convert.ToInt32(input[0]); // məsələn: 2
+            int s = Convert.ToInt32(input[1]); // məsələn: 5
+
+            int start = (int)Math.Pow(10, n - 1);
+            int end = (int)Math.Pow(10, n) - 1;
+
+            // Əgər n = 1 isə, 0-dan başlayaq
+            if (n == 1)
+                start = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                int sum = 0;
+                int temp = i;
+
+                while (temp > 0)
+                {
+                    sum += temp % 10;
+                    temp /= 10;
+                }
+
+                if (sum == s)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+
+}
